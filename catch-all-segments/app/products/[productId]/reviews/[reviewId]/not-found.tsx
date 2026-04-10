bd1bcff2715a237e33585cdace35ea0de61697aa
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2026-04-10 21:12:01 +0800

    more examples of not found page.

--- catch-all-segments/app/products/[productId]/reviews/[reviewId]/not-found.tsx
+++ catch-all-segments/app/products/[productId]/reviews/[reviewId]/not-found.tsx
@@ -1,15 +1,23 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function NotFound() {
+  const pathname = usePathname();
+  const productId = pathname.split("/")[2];
+  const reviewId = pathname.split("/")[4];
+
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-center gap-6 py-32 px-16 bg-white text-center dark:bg-black">
         <h1 className="text-6xl font-bold text-black dark:text-zinc-50">404</h1>
         <h2 className="text-2xl font-semibold text-zinc-800 dark:text-zinc-200">
-          Page Not Found
+          Review {reviewId} not found for product {productId}
         </h2>
         <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-          Sorry, the page you are looking for does not exist or has been moved.
+          Sorry, the review you are looking for does not exist or has been
+          moved.
         </p>
         <Link
           href="/"
