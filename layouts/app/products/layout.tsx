2126179d045ae8e1a158b57d1131d44362b3a4e7
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2026-04-11 00:40:56 +0800

    Added layouts examples

---
+++ layouts/app/products/layout.tsx
@@ -0,0 +1,12 @@
+export default function ProductsLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div>
+      <h1>Products Layout</h1>
+      {children}
+    </div>
+  );
+}
